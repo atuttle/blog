15a122cd378d222c845576b66df417206f00ec34
Adam Tuttle <adamtuttlecodes@gmail.com>
2021-07-30 09:09:43 -0400

    tweak post list page

--- .eleventy.js
+++ .eleventy.js
@@ -29,6 +29,10 @@ module.exports = function (eleventyConfig) {
 		);
 	});
 
+	eleventyConfig.addFilter("monthDay", (dateObj) => {
+		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("dd LLL");
+	});
+
 	// https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
 	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
 		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
